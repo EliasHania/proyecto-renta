fccf12f3c25221ffa91ead32313bbaf95453806a
EliasHania <eliasnumero1@gmail.com>
2025-05-24 14:07:07 +0200

    Usa HTTPS en callbackURL para producción

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,12 +2,16 @@ import passport from "passport";
 import GoogleStrategy from "passport-google-oauth20";
 import User from "../models/User.js";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: isProduction
+        ? "https://proyecto-renta.onrender.com/api/auth/google/callback"
+        : "http://localhost:5000/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -29,7 +33,7 @@ passport.use(
   )
 );
 
-// Serializar y deserializar usuario para la sesión
+// Serializar y deserializar usuario
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
