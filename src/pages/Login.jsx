c7fc10fd2e6ce886d4cd108674a704f9762e4b40
EliasHania <eliasnumero1@gmail.com>
2025-05-24 14:02:32 +0200

    Favicon google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -109,7 +109,7 @@ const Login = () => {
             className="inline-flex items-center justify-center px-4 py-2 bg-white border border-gray-300 rounded text-sm text-gray-700 hover:bg-gray-100 transition"
           >
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
               alt="Google"
               className="w-5 h-5 mr-2"
             />
